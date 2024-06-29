a050b06e40721b723c981ae0d9f1e409cec30782
Nebojsa Lukic <nlukic129@gmail.com>
2024-06-29 13:50:54 +0200

    Refactor NavigationToggle component and improve animation logic

--- src/components/NavigationToggle.tsx
+++ src/components/NavigationToggle.tsx
@@ -9,24 +9,30 @@ export const NavigationToggle = ({ isOpen }: any) => {
   const { hospitalityVenues } = useContext(StorageContext);
 
   return (
-    <motion.div
-      initial={{ opacity: 0, left: -300, zIndex: 100, position: "absolute", top: 100 }}
-      animate={isOpen ? { opacity: 1, left: 0 } : { opacity: 0, left: -300 }}
-      transition={{ duration: 0.1 }}
-      className="w-72 overflow-hidden h-50 z-40"
-    >
-      <div className="border-b border-typography pb-10">
-        <NavigationLink tableType={tableType.tables} />
-        <NavigationLink tableType={tableType.waiters} />
-        <NavigationLink tableType={tableType.menu} />
-        <NavigationLink tableType={tableType.messages} />
-      </div>
-      <div className="pt-10">
-        <p className="text-center mb-7">Bars&Restaurants</p>
-        {hospitalityVenues.map((venue, index) => (
-          <HospitalityVenueLink venueId={venue.id} key={index} />
-        ))}
-      </div>
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ opacity: 0, left: -300, zIndex: 100, position: "absolute", top: 100 }}
+        animate={
+          isOpen
+            ? { opacity: 1, left: 0, transition: { duration: 0.1, delay: 0.2 } }
+            : { opacity: 0, left: -300, transition: { duration: 0.1, delay: 0 } }
+        }
+        className="w-72 overflow-hidden h-50 z-40"
+      >
+        <div className="border-b border-typography pb-10">
+          <NavigationLink tableType={tableType.tables} />
+          <NavigationLink tableType={tableType.waiters} />
+          <NavigationLink tableType={tableType.menu} />
+          <NavigationLink tableType={tableType.messages} />
+        </div>
+
+        <div className="pt-10">
+          <p className="text-center mb-7">Bars&Restaurants</p>
+          {hospitalityVenues.map((venue, index) => (
+            <HospitalityVenueLink venueId={venue.id} key={index} />
+          ))}
+        </div>
+      </motion.div>
+    </>
   );
 };
