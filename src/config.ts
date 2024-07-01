0e57d302068acc1336cd591525e2c931f75287d5
Nebojsa Lukic <nlukic129@gmail.com>
2024-07-01 22:19:14 +0200

    Update API address in config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const env = process.env.REACT_APP_ENV || "development";
 
-export const apiAddress = process.env.REACT_APP_API_ADDRESS || "http://localhost:8080";
+export const apiAddress = process.env.REACT_APP_API_ADDRESS || "https://orderup-staticdataapi.onrender.com";
 export const jwtSecret = process.env.REACT_APP_JWT_SECRET || "";
