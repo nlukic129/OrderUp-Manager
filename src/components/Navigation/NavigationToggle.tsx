f1d0b1859af3f6ab142797015e2dd5af2b443f29
Nebojsa Lukic <nlukic129@gmail.com>
2024-07-08 21:58:52 +0200

    Update NavigationToggle component styling to use full screen width and adjust the left position for animation

--- src/components/Navigation/NavigationToggle.tsx
+++ src/components/Navigation/NavigationToggle.tsx
@@ -12,7 +12,7 @@ export const NavigationToggle = ({ isOpen, toggle }: any) => {
     <>
       <motion.div
         initial={{ opacity: 0, zIndex: 49, position: "absolute", top: 0, left: 0 }}
-        animate={isOpen ? { opacity: 1, left: 0, transition: { duration: 0 } } : { opacity: 0, left: -400, transition: { duration: 0.5 } }}
+        animate={isOpen ? { opacity: 1, left: 0, transition: { duration: 0 } } : { opacity: 0, left: -800, transition: { duration: 0.5 } }}
         className="w-full h-screen backdrop-blur-sm z-49 overflow-hidden"
         onClick={toggle}
       ></motion.div>
